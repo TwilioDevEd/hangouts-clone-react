5263e12d71644585d2216285c09b1a87fec878b2
Safia Abdalla <safia@safia.rocks>
2017-02-06 07:56:31 -0600

    Add application routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, Route, browserHistory } from 'react-router';
+
+import configureStore from './store/configureStore';
+import preloadedState from './store/preloadedState';
+
+import App from './components/app';
+import Login from './components/login';
+import Main from './components/main';
+import VideoChat from './components/video-chat';
+
+const store = configureStore(preloadedState);
+const history = syncHistoryWithStore(browserHistory, store);
+
+const routes = (
+  <Route path="/" component={App}>
+    <Route path="/login" component={Login}/>
+    <Route path="/:username" component={Main}/>
+    <Route path="/:username/video/:id" component={VideoChat}/>
+  </Route>
+);
 
 ReactDOM.render(
-  <Provider store={}>
-    <Router history={} routes={}/>
+  <Provider store={store}>
+    <Router history={history} routes={routes}/>
   </Provider>,
 document.getElementById('root'));
