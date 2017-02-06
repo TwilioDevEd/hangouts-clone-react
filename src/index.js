6894967d0b985376c90d394a564743f92f0522f3
Safia Abdalla <safia@safia.rocks>
2017-02-05 19:28:52 -0600

    Initialize project scaffolding

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+
+ReactDOM.render(
+  <Provider store={}>
+    <Router history={} routes={}/>
+  </Provider>,
+document.getElementById('root'));
