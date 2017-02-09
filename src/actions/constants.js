a81a95e51042b36c4ac7488b0955eb44f19ce343
Safia Abdalla <safia@safia.rocks>
2017-02-08 20:23:59 -0600

    Add action creators

---
+++ src/actions/constants.js
@@ -0,0 +1,3 @@
+export const SET_USER_TOKEN = 'SET_USER_TOKEN';
+export const SET_USER_ID = 'SET_USER_ID';
+export const SET_USER_NAME = 'SET_USER_NAME';
---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import * as constants from './constants';
+
+export const setUserToken = (token) => {
+  return {
+    type: constants.SET_USER_TOKEN,
+    token,
+  };
+};
+
+export const setUserId = (id) => {
+  return {
+    type: constants.SET_USER_ID,
+    id,
+  };
+};
+
+export const setUserName = (username) => {
+  return {
+    type: constants.SET_USER_NAME,
+    username,
+  };
+};
