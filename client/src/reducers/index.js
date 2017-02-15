00300c51c4d9d504e20580cc8f857b68efbe882e
Safia Abdalla <safia@safia.rocks>
2017-02-14 23:28:03 -0600

    Remove peripherals reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import user from './user';
 import room from './room';
-import peripherals from './peripherals';
 
 const rootReducer = combineReducers({
   user,
+  room,
   routing
 });
 
