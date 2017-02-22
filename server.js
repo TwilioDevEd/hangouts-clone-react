dc6cd5bff9f41721bea89189b3cceb3433dd785a
Safia Abdalla <safia@safia.rocks>
2017-02-22 01:42:58 -0600

    Check for valid payload on /token

--- server.js
+++ server.js
@@ -20,38 +20,41 @@ app.use(bodyParser.json());
 app.post('/token', (request, response) => {
   const body = request.body;
 
-  // Validate Goolge ID token sent from front-end 
-  client.verifyIdToken(body.tokenId, process.env.GOOGLE_CLIENT_ID, (e, login) => {
-    const payload = login.getPayload();
-    const userId = payload.sub;
-    if (userId === body.googleId) {
-      const token = new AccessToken(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_API_KEY,
-        process.env.TWILIO_API_SECRET
-      );
+  if (!body.tokenId && !body.googleId) {
+    response.status(500);
+    response.send({
+      error: 'Invalid payload provided!',
+    });
+  } else {
+    // Validate Goolge ID token sent from front-end 
+    client.verifyIdToken(body.tokenId, process.env.GOOGLE_CLIENT_ID, (e, login) => {
+      const payload = login.getPayload();
+      const userId = payload.sub;
+      if (userId === body.googleId) {
+        const token = new AccessToken(
+          process.env.TWILIO_ACCOUNT_SID,
+          process.env.TWILIO_API_KEY,
+          process.env.TWILIO_API_SECRET
+        );
 
-      token.identity = body.googleId;
+        token.identity = body.googleId;
 
-      const grant = new VideoGrant();
-      grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
-      token.addGrant(grant);
-      
-      response.send({
-        identity: body.googleId,
-        token: token.toJwt(),
-      });
-    } else {
-      response.status(500);
-      response.send({
-        error: 'Authentication was unsuccessful!',
-      });
-    }
-  });
-});
-
-app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+        const grant = new VideoGrant();
+        grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+        token.addGrant(grant);
+        
+        response.send({
+          identity: body.googleId,
+          token: token.toJwt(),
+        });
+      } else {
+        response.status(500);
+        response.send({
+          error: 'Authentication was unsuccessful!',
+        });
+      }
+    });
+  }
 });
 
 app.listen(3001, () => {
