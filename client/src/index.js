4ef789d8608b23c030e9190936a8c5bc5325f989
Safia Abdalla <safia@safia.rocks>
2017-02-22 00:44:32 -0600

    Close #6

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Router, Route, browserHistory } from 'react-router';
-import { persistStore } from 'redux-persist';
 
 import configureStore from './store/configureStore';
 import preloadedState from './store/preloadedState';
